eeb89cf3583fff77545b029ade26fe887798d94d
Abraham Narvaez Valera <abrahamnarvaezv@gmail.com>
2020-11-16 01:04:57 -0500

    agregando react-router-dom, login.js y login.css

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/login.css';
+
+const Login = () => {
+    return ( 
+        <>
+        <div>Login</div>
+        </>
+     );
+}
+ 
+export default Login;
