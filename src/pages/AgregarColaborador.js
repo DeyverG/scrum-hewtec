22b22862a4c54615a49ca15ed11bf48ee5cd6bc4
Abraham Narvaez Valera <abrahamnarvaezv@gmail.com>
2020-11-16 14:38:07 -0500

    cambios en el app.js

---
+++ src/pages/AgregarColaborador.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AgregarColaborador = () => {
+    return ( 
+        <>
+        <div>Add colaborator</div>
+        </>
+     );
+}
+ 
+export default AgregarColaborador;
