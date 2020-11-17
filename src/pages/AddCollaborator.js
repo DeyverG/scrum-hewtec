92dd4a0ca96e840ad04f25c1631213d3de7c602a
Abraham Narvaez Valera <abrahamnarvaezv@gmail.com>
2020-11-17 14:03:52 -0500

    Vista Add Collaborator terminada

--- src/pages/AddCollaborator.js
+++ src/pages/AddCollaborator.js
@@ -4,7 +4,18 @@ import '../assets/styles/addCollaborator.css';
 const AddCollaborator = () => {
     return ( 
         <>
-        Add Collaborator
+        <div className="containerAddColl" >
+            <div className="modalAddColl" >
+                <h2 className="titleAddColl" >Add Collaborator</h2>
+                <div className="divInputAddColl" >
+                    <p className="pAddColl" >Enter email</p>
+                    <input className="inputAddColl" ></input>
+                </div>
+                <div className="divButtonAddColl" >
+                    <button className="btn btn-lg buttonAddColl" ><b>Accept</b></button>
+                </div>
+            </div>
+        </div>
         </>
      );
 }
