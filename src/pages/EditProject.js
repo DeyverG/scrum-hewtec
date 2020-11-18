7e61d6c45808a3eb11e81caf8e5a320471839963
Abraham Narvaez Valera <abrahamnarvaezv@gmail.com>
2020-11-18 04:12:00 -0500

    EditProject casi terminado

--- src/pages/EditProject.js
+++ src/pages/EditProject.js
@@ -4,18 +4,32 @@ import '../assets/styles/editProject.css';
 const EditProject = () => {
     return ( 
         <>
-        <div className="containerEditPro" >
-            <div className="modalEditPro" >
-                <div className="divH1EditPro" >
-                    <h2>Project Name</h2>
-                    <input></input>
-                    <input></input>
-                </div>
-                <div className="divH2EditPro" >
-                    <h2>Collaborators</h2>
+        <form>
+            <div className="containerEditPro" >
+                <div className="row modalEditPro" >
+                    <div className="form-group col-md-6" >
+                        <h5 className="titleEditPro" >Project Name</h5>
+                        <div className="input-group mb-3" >
+                            <input type="text" className="form-control inputEditPro" ></input>
+                            <div className="input-group-append iconoEditPro" >
+                                <span class="input-group-text spanEditPro" ><i class="fas fa-edit"></i></span>
+                            </div>
+                        </div>
+                        <h5 className="titleEditPro" >Description</h5>
+                        <textarea className="form-control textEditPro" ></textarea>
+                        <div className="divButtonEditPro" >
+                            <button className="btn buttonEditPro" >Save</button>
+                        </div>
+                    </div>
+                    <div className="form-group col-md-6" >
+                        <h5 className="titleEditPro" >Collaborators</h5>
+                        <div className="collEditPro" >
+                            Colaboradores
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </form>
         </>
      );
 }
