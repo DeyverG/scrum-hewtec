f175ce538ec0412b661793cb61f89d2f4e460ac0
DeyverG <34787440+DeyverG@users.noreply.github.com>
2020-11-17 21:47:54 -0500

    vista myProjects, Header y Footer terminada

--- src/pages/MyProjects.js
+++ src/pages/MyProjects.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 import '../assets/styles/myProjects.css';
+import Project from '../components/Project';
+
 
 const MyProjects = () => {
-    return ( 
+    return (
         <>
+            <Header />
+
+            <div className="tamaño scrollDiv">
+                <div className="contenedorProject">
+                    <div className="project">
+                        <p className="fas fa-folder-plus projectName iconoFolder"></p>
+                        <p className="projectName createProject">Create Project</p>
+                    </div>
+                    <Project />
+                    <Project />
+                    <Project />
+                    <Project />
+                    <Project />
+                    <Project />
+                    <Project />
+                    <Project />
+                    <Project />
+                    <Project />
+                    <Project />
+                    <Project />
+                    <Project />
+                    <Project />
+                    <Project />
+                </div>
+
+            </div>
         </>
-     );
+    );
 }
- 
+
 export default MyProjects;
