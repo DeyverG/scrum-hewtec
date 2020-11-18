2c939ab3c1b76891099e372fad2a5eb809c7c8f8
Abraham Narvaez Valera <abrahamnarvaezv@gmail.com>
2020-11-18 00:32:30 -0500

    cambios sin terminar

--- src/pages/EditProject.js
+++ src/pages/EditProject.js
@@ -4,6 +4,18 @@ import '../assets/styles/editProject.css';
 const EditProject = () => {
     return ( 
         <>
+        <div className="containerEditPro" >
+            <div className="modalEditPro" >
+                <div className="divH1EditPro" >
+                    <h2>Project Name</h2>
+                    <input></input>
+                    <input></input>
+                </div>
+                <div className="divH2EditPro" >
+                    <h2>Collaborators</h2>
+                </div>
+            </div>
+        </div>
         </>
      );
 }
