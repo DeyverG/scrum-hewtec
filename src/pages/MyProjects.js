9344baa53be70afbd4800f922ddeedb66deac5fe
DeyverG <34787440+DeyverG@users.noreply.github.com>
2020-11-19 20:29:05 -0500

    implementando js a la vista project y createProject

--- src/pages/MyProjects.js
+++ src/pages/MyProjects.js
@@ -1,36 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import '../assets/styles/myProjects.css';
 import Project from '../components/Project';
+import { Link } from 'react-router-dom';
 
 
 const MyProjects = () => {
+
+    const [projects, setProjects] = useState([]);
+
+    const consultarDB = async () => {
+        const consulta = await JSON.parse(localStorage.getItem('projects'));
+        if (!consulta) {
+            localStorage.setItem('projects', '[]');
+        } else {
+            setProjects(consulta)
+        }
+    }
+
+    useEffect(() => {
+        consultarDB()
+    }, [])
+
     return (
         <>
             <Header />
-
             <div className="tamañoProjects scrollDiv">
+
                 <div className="contenedorProject">
-                    <div className="project">
-                        <p className="fas fa-folder-plus projectName iconoFolder"></p>
-                        <p className="projectName createProject">Create Project</p>
-                    </div>
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
+                    <Link to="/CreateProject" className="marginLink">
+                        <div className="project">
+                            <p className="fas fa-folder-plus projectName iconoFolder"></p>
+                            <p className="projectName createProject">Create Project</p>
+                        </div>
+                    </Link>
+                    {projects.map(project => (
+                        <Project
+                            key={project.id}
+                            id={project.id}
+                            name={project.name}
+                            description={project.description}
+                        />
+                    ))}
                 </div>
 
             </div>
