1283dc3ed060d489f67ad2d4e891c26d0bfd741f
alejaimesperez <49459553+alejandrajaimes@users.noreply.github.com>
2020-11-19 22:44:00 -0500

    arreglos

--- src/pages/ProjectView.js
+++ src/pages/ProjectView.js
@@ -8,36 +8,36 @@ const ProjectView = () => {
     <>
       <Header />
       <div className="contenido8">
-      <div className="container ">
-        
+        <div className="container">
+
           <div className="row">
-    <div className="col-6 iconos_grid">
-    <i className="fas fa-user-plus color_icon"></i>
-    </div>
-    
-    <div className="col-6 iconos_grid1">
-    <i className="fas fa-cogs color_icon"></i>
-    
-    </div>
-  </div>
-  
-          <div className="row">
-            <div className="col-11 cards_project">
-              <Cards/>
-              <Cards/>
-              <Cards/>
-              <div className="cards_general">
-             <div className="contenido_new2">
-             <i class="fas fa-plus-circle icono_mas"></i>
-             <p className="contenido_new" >New Section</p>
-             </div>
-           </div>
+            <div className="col-6 iconos_grid">
+              <i className="fas fa-user-plus color_icon"></i>
+            </div>
+
+            <div className="col-6 iconos_grid1">
+              <i className="fas fa-cogs color_icon"></i>
+
             </div>
           </div>
-</div>
-      
-      
-                
+
+          <div className="row">
+            <div className="col-11 cards_project">
+              <Cards />
+              <Cards />
+              <Cards />
+              <div className="cards_general">
+                <div className="contenido_new2">
+                  <i class="fas fa-plus-circle icono_mas"></i>
+                  <p className="contenido_new" >New Section</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
       </div>
     </>
   );
