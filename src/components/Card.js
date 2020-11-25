20ab14439dea97af02093a7cbe37384383eaf603
DeyverG <34787440+DeyverG@users.noreply.github.com>
2020-11-25 00:28:00 -0500

    Funciones de las tareas, creacion de tarea, eliminar y editar

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,52 +1,135 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import '../assets/styles/card.css';
 import Eq from '../assets/img/eq.jpg';
 import Task from '../components/Task';
 import Modal from 'react-bootstrap/Modal';
+import { nanoid } from 'nanoid'
 
-const Card = ({name}) => {
+const Card = ({ id, name, sections, setSections, setBandera, idProject }) => {
 
   // Modal New Task
   const [showNewTask, setShowNewTask] = useState(false);
   const handleCloseNewTask = () => setShowNewTask(false);
   const handleShowNewTask = () => setShowNewTask(true);
 
+  // Modal Delete Section
+  const [showDeleteTask, setDeleteTask] = useState(false);
+  const handleCloseDeleteTask = () => setDeleteTask(false);
+  const handleShowDeleteTask = () => setDeleteTask(true);
+
+  const [tasks, setTasks] = useState([]);
+  const [newtask, setNewTask] = useState({
+    id: nanoid(),
+    idSection: id,
+    name: '',
+    description: ''
+  })
+  const handleDelete = e => {
+    const filtrado = sections.filter(section => section.id !== e);
+    localStorage.setItem(`${idProject}`, JSON.stringify(filtrado));
+    setSections(filtrado);
+    setBandera(true)
+    handleCloseDeleteTask()
+  }
+
+  const consultaDBTask = () => {
+    const peticion = JSON.parse(localStorage.getItem('task'));
+    if (!peticion) {
+      localStorage.setItem('task', '[]')
+      return
+    }
+    const filtrado = peticion.filter(result => result.idSection === id)
+    setTasks(filtrado)
+  }
+
+  const handleChange = e => {
+    setNewTask({
+      ...newtask,
+      [e.target.name]: e.target.value
+    })
+  }
+  const handleSubmit = () => {
+
+    if (newtask.name.trim() === '' || newtask.description.trim() === '') {
+      alert('completa esa monda, malparido');
+      return
+    }
+    setNewTask({
+      id: nanoid()
+    })
+    const taskBD = JSON.parse(localStorage.getItem("task"));
+    taskBD.push(newtask);
+    localStorage.setItem('task', JSON.stringify(taskBD));
+    setBandera(true)
+    setSections([])
+    handleCloseNewTask()
+  }
+
+  useEffect(() => {
+    consultaDBTask()
+  }, [])
+
   return (
     <>
       <div className="cards_general">
-        <img className="imgaeq" src={Eq} alt="" />
+        <img className="imgaeq" src={Eq} id={id} alt="" onClick={handleShowDeleteTask} />
         <p>{name}</p>
-        <hr/>
+        <hr />
         <button type="submit" className="btn botton_Edit1" onClick={handleShowNewTask} >
           New Task
         </button>
-        <Modal show={showNewTask} onHide={handleCloseNewTask}>
-          <Modal.Header closeButton>
-            <Modal.Title>NEW TASK</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div className="divModalBodyNewTask" >
-              <h6>Name</h6>
-              <div className="divInputModalBodyNewTask" >
-                <input className="form-control inputEditModalNewTask" ></input>
-              </div>
-
-              <h6>Description</h6>
-              <div className="divTextareaModalBodyNewTask" >
-                <textarea className="form-control textareaEditModalNewTask" ></textarea>
-              </div>
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <button type="button" className="btn btn-primary btnEditModalSaveNewTask" onClick={handleCloseNewTask} >
-              <h5>Save</h5>
-            </button>
-          </Modal.Footer>
-        </Modal>
-
-
-        <Task/>
+        {tasks.map(task => (
+          <Task
+            id={task.id}
+            idSection={task.idSection}
+            name={task.name}
+            description={task.description}
+            setBandera={setBandera}
+            setSections={setSections}
+            sections={sections}
+          />
+        ))}
       </div>
+
+      <Modal show={showNewTask} onHide={handleCloseNewTask}>
+        <Modal.Header closeButton>
+          <Modal.Title>NEW TASK</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="divModalBodyNewTask" >
+            <h6>Name</h6>
+            <div className="divInputModalBodyNewTask" >
+              <input className="form-control inputEditModalNewTask" onChange={handleChange} name="name"></input>
+            </div>
+
+            <h6>Description</h6>
+            <div className="divTextareaModalBodyNewTask" >
+              <textarea className="form-control textareaEditModalNewTask" name="description" onChange={handleChange}></textarea>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button type="button" className="btn btn-primary btnEditModalSaveNewTask" onClick={handleSubmit} >
+            <h5>Save</h5>
+          </button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showDeleteTask} onHide={handleCloseDeleteTask}>
+        <Modal.Header closeButton>
+          <Modal.Title>DELETE SECTION</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="divModalBodyNewTask" >
+            <h6>Delete</h6>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button type="button" className="btn btn-primary btnEditModalSaveNewTask" onClick={() => handleDelete(id)} >
+            <h5>Delete</h5>
+          </button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../assets/styles/task.css';
 import Modal from 'react-bootstrap/Modal';
 
-const Task= () => {
+const Task = ({ id, idSection, name, description, setBandera, setSections, sections }) => {
 
   // Modal Edit
   const [showEdit, setShowEdit] = useState(false);
@@ -14,77 +14,194 @@ const Task= () => {
   const handleCloseDelete = () => setShowDelete(false);
   const handleShowDelete = () => setShowDelete(true);
 
+  // Modal info
+  const [showinfo, setShowinfo] = useState(false);
+  const handleCloseinfo = () => setShowinfo(false);
+  const handleShowinfo = () => setShowinfo(true);
+
+  const [editTask, setEditTask] = useState({
+    id: id,
+    idSection: idSection,
+    name: name,
+    description: description,
+  })
+
+  const handleChange = e => {
+    setEditTask({
+      ...editTask,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSave = () => {
+    if (editTask.name.trim() === '' || editTask.description.trim() === '') {
+      alert('que llenes esa monda, care verga');
+      return
+    }
+    const taskDB = JSON.parse(localStorage.getItem('task'));
+    const change = taskDB.map(task => task.id === editTask.id ? {
+      id: editTask.id,
+      idSection: editTask.idSection,
+      name: editTask.name,
+      description: editTask.description
+    } : task)
+    localStorage.setItem('task', JSON.stringify(change));
+    setBandera(true)
+    setSections([])
+    handleCloseEdit()
+  }
+  const handleDelete = () => {
+    const taskDB = JSON.parse(localStorage.getItem('task'));
+    const change = taskDB.filter(task => task.id !== editTask.id);
+    localStorage.setItem('task', JSON.stringify(change));
+    setBandera(true)
+    setSections([])
+    handleCloseDelete()
+  }
+
+  const handleMoveTaskNext = () => {
+    const taskDB = JSON.parse(localStorage.getItem('task'));
+    const pos = sections.findIndex(section => section.id === editTask.idSection);
+    const newPos = sections[(pos + 1)].id
+    const change = taskDB.map(task => task.id === editTask.id ? {
+      id: editTask.id,
+      idSection: newPos,
+      name: editTask.name,
+      description: editTask.description
+    } : task)
+    localStorage.setItem('task', JSON.stringify(change));
+    setBandera(true)
+    setSections([])
+    handleCloseEdit()
+  }
+
+  const handleMoveTaskPrevious = () => {
+    const taskDB = JSON.parse(localStorage.getItem('task'));
+    const pos = sections.findIndex(section => section.id === editTask.idSection);
+    const newPos = sections[(pos - 1)].id
+    const change = taskDB.map(task => task.id === editTask.id ? {
+      id: editTask.id,
+      idSection: newPos,
+      name: editTask.name,
+      description: editTask.description
+    } : task)
+    localStorage.setItem('task', JSON.stringify(change));
+    setBandera(true)
+    setSections([])
+    handleCloseEdit()
+  }
+
   return (
     <>
       <div className="contenido-tarea" >
         <div className="texto_img" >
-          <p>Name Task</p>
+          <p onClick={handleShowinfo} className="titulo-task">{name}</p>
         </div>
         <div className="btn-group dropright icon_Task" >
-          <button type="button" className="btn btnModal" id="dropdownMenu" 
+          <button type="button" className="btn btnModal" id="dropdownMenu"
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
             <i className="fas fa-ellipsis-v"></i>
           </button>
           <div className="dropdown-menu divIcons" aria-labelledby="dropdown" >
+            {sections[0].id === idSection
+              ?
+              null
+              :
+              <button type="button" className="btn btnLeft" onClick={handleMoveTaskPrevious}>
+                <i className="fas fa-chevron-circle-left"></i>
+              </button>
+            }
 
-            <button type="button" className="btn btnLeft" >
-              <i className="fas fa-chevron-circle-left"></i>
-            </button>
 
             <button type="button" className="btn btnEditTask" onClick={handleShowEdit} >
               <i className="fas fa-edit imgeditar_eliminar" ></i>
             </button>
-            <Modal show={showEdit} onHide={handleCloseEdit}>
-              <Modal.Header closeButton>
-                <Modal.Title>EDIT TASK</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <div className="divModalBodyTask" >
-                  <h6>Name</h6>
-                  <div className="divInputModalBodyTask" >
-                    <input className="form-control inputEditModalTask" ></input>
-                  </div>
 
-                  <h6>Description</h6>
-                  <div className="divTextareaModalBodyTask" >
-                    <textarea className="form-control textareaEditModalTask" ></textarea>
-                  </div>
-                </div>
-              </Modal.Body>
-              <Modal.Footer>
-                <button type="button" className="btn btn-primary btnEditModalSaveTask" onClick={handleCloseEdit} >
-                  <h5>Save</h5>
-                </button>
-              </Modal.Footer>
-            </Modal>
 
             <button type="button" className="btn btnDeleteTask" onClick={handleShowDelete} >
               <i className="fas fa-trash-alt imgeditar_eliminar1"></i>
             </button>
-            <Modal show={showDelete} onHide={handleCloseDelete} >
-              <Modal.Header closeButton >
-                <Modal.Title>DELETE TASK</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <h6>Are you sure to delete task?</h6>
-              </Modal.Body>
-              <Modal.Footer>
-                <button type="button" className="btn btn-primary btnDeleteModalCancelTask" onClick={handleCloseDelete} >
-                  <h5>Cancel</h5>
-                </button>
-                <button type="button" className="btn btn-danger btnDeleteModalConfirmTask" onClick={handleCloseDelete} >
-                  <h5>Confirm</h5>
-                </button>
-              </Modal.Footer>
-            </Modal>
+            {sections[(sections.length - 1)].id === idSection
+              ?
+              null
+              :
+              <button type="button" className="btn btnRight" onClick={handleMoveTaskNext}>
+                <i className="fas fa-chevron-circle-right"></i>
+              </button>
+            }
 
-            <button type="button" className="btn btnRight" >
-              <i className="fas fa-chevron-circle-right"></i>
-            </button>
 
           </div>
         </div>
       </div>
+
+      {/* Edit Modal */}
+      <Modal show={showEdit} onHide={handleCloseEdit}>
+        <Modal.Header closeButton>
+          <Modal.Title>EDIT TASK</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="divModalBodyTask" >
+            <h6>Name</h6>
+            <div className="divInputModalBodyTask" >
+              <input className="form-control inputEditModalTask" name="name" value={editTask.name} onChange={handleChange}></input>
+            </div>
+
+            <h6>Description</h6>
+            <div className="divTextareaModalBodyTask" >
+              <textarea className="form-control textareaEditModalTask" name="description" value={editTask.description} onChange={handleChange}></textarea>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button type="button" className="btn btn-primary btnEditModalSaveTask" onClick={handleSave} >
+            <h5>Save</h5>
+          </button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Delete Modal */}
+      <Modal show={showDelete} onHide={handleCloseDelete} >
+        <Modal.Header closeButton >
+          <Modal.Title>DELETE TASK</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h6>Are you sure to delete task?</h6>
+        </Modal.Body>
+        <Modal.Footer>
+          <button type="button" className="btn btn-primary btnDeleteModalCancelTask" onClick={handleCloseDelete} >
+            <h5>Cancel</h5>
+          </button>
+          <button type="button" className="btn btn-danger btnDeleteModalConfirmTask" onClick={handleDelete} >
+            <h5>Confirm</h5>
+          </button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Info Modal */}
+      <Modal show={showinfo} onHide={handleCloseinfo}>
+        <Modal.Header closeButton>
+          <Modal.Title>TASK INFORMATION</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="divModalBodyTask" >
+            <h4>Name</h4>
+            <div className="divInputModalBodyTask" >
+              <input className="form-control inputEditModalTask" disabled value={name}></input>
+            </div>
+
+            <h4>Description</h4>
+            <div className="divTextareaModalBodyTask" >
+              <textarea className="form-control textareaEditModalTask" disabled value={description}></textarea>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button type="button" className="btn btn-primary btnEditModalSaveTask" onClick={handleCloseinfo} >
+            <h5>Close</h5>
+          </button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
